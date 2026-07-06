ef3ee4f59cf387a9ba5d74999f5329b4a568c8a7
kodumurisandeep14 <kodumurisandeep14@gmail.com>
2026-07-06 12:15:42 -0400

    feat: implement oauth2.o

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.core.oauth2 import get_current_user
+
 from app.db.session import get_db
 
 from app.schemas.user_schema import (
@@ -46,3 +48,15 @@ def login(
     db: Session = Depends(get_db)
 ):
     return login_user(db, user)
+
+# ----------------------------
+# Current User
+# ----------------------------
+@router.get(
+    "/me",
+    response_model=UserResponse
+)
+def get_me(
+    current_user=Depends(get_current_user)
+):
+    return current_user
